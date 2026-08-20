70ea26fcb9a46745d3cc8bde5135ca9785b4838a
Akash2-2kumar <106278593+Akash2-2kumar@users.noreply.github.com>
2026-08-20 18:30:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstChar(text) {
   // your code here
+  text = text.trim();
+
+    if(text.length === 0){
+        return "";
+    }
   let ans=text.trim().charAt(0);
 	if(ans==' '){
 		ans="";
